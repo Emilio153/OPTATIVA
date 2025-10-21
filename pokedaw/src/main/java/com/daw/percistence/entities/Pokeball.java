a17cbee806a606b7ff450e65ef39e703ec684330
Emilio153 <tukaali@hotmail.com>
2025-10-21 09:18:09 +0200

    poke

---
+++ pokedaw/src/main/java/com/daw/percistence/entities/Pokeball.java
@@ -0,0 +1,6 @@
+package com.daw.percistence.entities;
+
+public enum Pokeball {
+	POKEBALL, SUPERBALL, ULTRABALL
+
+}
---
+++ pokedaw/src/main/java/com/daw/percistence/repositories/PokemonRepository.java
@@ -0,0 +1,5 @@
+package com.daw.percistence.repositories;
+
+public interface PokemonRepository extends List {
+
+}
