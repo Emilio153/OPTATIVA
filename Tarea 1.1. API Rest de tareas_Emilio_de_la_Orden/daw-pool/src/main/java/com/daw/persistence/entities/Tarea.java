4a8a92452e961da0df4a472a4d69d4c263872595
Emilio153 <eordcru000@g.educaand.es>
2025-10-21 19:34:51 +0200

    asdas

---
+++ Tarea 1.1. API Rest de tareas_Emilio_de_la_Orden/daw-pool/src/main/java/com/daw/persistence/entities/Tarea.java
@@ -0,0 +1,35 @@
+package com.daw.persistence.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tarea")
+@Getter
+@Setter
+public class Tarea {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_tarea")
+	private int idTarea;
+	private String titulo;
+	private String descripcion;
+	@Column(name = "fecha_creacion")
+	private LocalDate fechaCreacion;
+	@Column(name = "fecha_vencimiento")
+	private LocalDate fechaVencimiento;
+	@Enumerated(value=EnumType.STRING)
+	private Estado estado;
+
+}
---
+++ Tarea 1.1. API Rest de tareas_Emilio_de_la_Orden/daw-pool/src/test/java/com/daw/daw_pool/DawPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.daw_pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
