1f77417451026e1fe4b56fa2b36f1a40b096c165
Emilio153 <eordcru000@g.educaand.es>
2025-10-23 23:51:58 +0200

    sdsada

--- pokedaw/src/main/java/com/daw/web/controllers/PokemonControllers.java
+++ pokedaw/src/main/java/com/daw/web/controllers/PokemonControllers.java
@@ -1,14 +1,11 @@
 package com.daw.web.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.daw.percistence.entities.Pokemon;
 import com.daw.services.PokemonService;
 
 @RestController
